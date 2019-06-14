3bf44984743988b96ae26b11bc5fb7fbe4e30655
Kurczaak <34412865+Kurczaak@users.noreply.github.com>
2019-06-14 09:31:30 +0200

    Add files via upload

---
+++ Function Calculator/isim/Project_System_Project_System_sch_tb_isim_beh.exe.sim/work/a_2139093499_4157108248.c
@@ -0,0 +1,85 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "D:/EDC1/Function Calculator/Project_System.vhf";
+
+
+
+static void work_a_2139093499_4157108248_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    unsigned char t3;
+    unsigned char t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+
+LAB0:    xsi_set_current_line(139, ng0);
+    t1 = (t0 + 1352U);
+    t2 = *((char **)t1);
+    t3 = *((unsigned char *)t2);
+    t4 = (t3 == (unsigned char)2);
+    if (t4 != 0)
+        goto LAB2;
+
+LAB4:    xsi_set_current_line(142, ng0);
+    t1 = xsi_get_transient_memory(16U);
+    memset(t1, 0, 16U);
+    t2 = t1;
+    memset(t2, (unsigned char)4, 16U);
+    t5 = (t0 + 2912);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t1, 16U);
+    xsi_driver_first_trans_fast_port(t5);
+
+LAB3:    t1 = (t0 + 2832);
+    *((int *)t1) = 1;
+
+LAB1:    return;
+LAB2:    xsi_set_current_line(140, ng0);
+    t1 = (t0 + 1192U);
+    t5 = *((char **)t1);
+    t1 = (t0 + 2912);
+    t6 = (t1 + 56U);
+    t7 = *((char **)t6);
+    t8 = (t7 + 56U);
+    t9 = *((char **)t8);
+    memcpy(t9, t5, 16U);
+    xsi_driver_first_trans_fast_port(t1);
+    goto LAB3;
+
+}
+
+
+extern void work_a_2139093499_4157108248_init()
+{
+	static char *pe[] = {(void *)work_a_2139093499_4157108248_p_0};
+	xsi_register_didat("work_a_2139093499_4157108248", "isim/Project_System_Project_System_sch_tb_isim_beh.exe.sim/work/a_2139093499_4157108248.didat");
+	xsi_register_executes(pe);
+}
